c213fc69d2f1cfd175df00e7579a87a7dc42aca5
phucgs <162669848+phucgs@users.noreply.github.com>
2024-05-02 21:32:40 +0700

    Update print-triangle.py

--- print-triangle.py
+++ print-triangle.py
@@ -1,1 +1,23 @@
+#Print triangle by python
+
+def triangle():
+    print("What's kind of shape do you want to print?")
+    print("1.Triangle 1")
+    print("2.Triangle 2")
+    question = str(input("> "))
+    if question == "1":
+        print("How many line do you want to print?")
+        line1 = int(input("> "))
+        print("#" * 15)
+        for i in range(1,line1):
+            print("*" * i,end="\n")
+        print("#" * 15)
+    elif question == "2":
+        print("How many line do you want to print?")
+        line2 = int(input("> "))
+        print("#" * 15)
+        for j in range(1,line2):
+            print("*" * (line2 -j))
+        print("#" * 15)  
+triangle()      
 
